115feed5b0c42e96d95f1a751b8f30f62c7a172e
SuYoungSong <prodraft@naver.com>
2021-11-08 20:52:06 +0900

    11/8
    11/8

--- src/application/bluemarble/testController.java
+++ src/application/bluemarble/testController.java
@@ -1,12 +1,17 @@
 package application.bluemarble;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import application.Main;
-import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Material;
 import javafx.scene.paint.PhongMaterial;
@@ -14,16 +19,16 @@ import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
-import javafx.stage.Stage;
 
-public class testController{
-	
-	void diceMake() {
-		// MeshView에 올릴 이미지
-		Image diffuseMap = new Image(Main.class.getResource("texture/dice.png").toExternalForm());
+
+public class testController implements Initializable{
+    @FXML private AnchorPane diceRolling;
+    SmartGroup group = new SmartGroup();
+    
+	public void diceMake() {
+		Image diffuseMap = new Image(Main.class.getResourceAsStream("texture/dice.png"));
 		Material diceSurface = new PhongMaterial(Color.WHITE, diffuseMap, null, null, null);
-		
-		// 이미지를 도형에 맞게 맵핑
+	
 		TriangleMesh mesh = new TriangleMesh();
 		float points[] = {				
 				0, 0, 50, 		// P0
@@ -35,10 +40,11 @@ public class testController{
 				0, 50, 0, 		// P6
 				50, 50, 0 		// P7
 		};
+
 		float[] tex = {
-				0.25f, 0, 					// T0 가로(왼쪽 부터) 4칸 > 25%
-				0.5f, 0, 					// T1 세로(상단 부터) 3칸 > 33%
-				0, 0.33f, 					// T2 왼쪽에 작성할때는 가로, 세로 순으로
+				0.25f, 0, 					// T0
+				0.5f, 0, 					// T1 
+				0, 0.33f, 					// T2
 				0.25f, 0.33f, 				// T3
 				0.5f, 0.33f, 				// T4
 				0.75f, 0.33f, 				// T5
@@ -51,6 +57,7 @@ public class testController{
 				0.25f, 1, 					// T12
 				0.50f, 1 					// T13
 		};
+
 		int[] faces = {
 				5,1, 4,0, 0,3,     //P5,T1 ,P4,T0  ,P0,T3
 				5,1, 0,3, 1,4,     //P5,T1 ,P0,T3  ,P1,T4
@@ -68,49 +75,70 @@ public class testController{
 		int[] faceSmoothingGroups = {
 				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 		};
-		
 		mesh.getFaceSmoothingGroups().addAll(faceSmoothingGroups);
 		mesh.getPoints().addAll(points);
 		mesh.getTexCoords().addAll(tex);
 		mesh.getFaces().addAll(faces);
-		// MeshView 생성
+	
 		MeshView meshView = new MeshView(mesh);
-		// MeshView 표면 설정
 		meshView.setMaterial(diceSurface);
-		// 그룹 설정
-		SmartGroup group = new SmartGroup();
+
+		
 		group.getChildren().add(meshView);
-		// 카메라 설정
-		PerspectiveCamera camera = new PerspectiveCamera();
-		// 그룹 초기 위치 설정
+//		PerspectiveCamera camera = new PerspectiveCamera();
+		diceRolling.getChildren().add(group);
+//		diceRolling.getScene().setCamera(camera);
 		group.translateXProperty().set(50);
 		group.translateYProperty().set(50);
-		group.translateZProperty().set(50);
-}
-
-//		// 키보드 컨트롤
-//		void onPressKey(KeyEvent e) {
-//			switch (e.getCode()) {
-//			case W:
-//				group.translateZProperty().set(group.getTranslateZ() + 100);
-//				break;
-//			case S:
-//				group.translateZProperty().set(group.getTranslateZ() - 100);
-//				break;
-//			case Q:
-//				group.rotateByX(10);
-//				break;
-//			case E:
-//				group.rotateByX(-10);
-//				break;
-//			case D:
-//				group.rotateByY(10);
-//				break;
-//			case A:
-//				group.rotateByY(-10);
-//				break;
-//			}
-//		scene.setCamera(camera);
+		group.translateZProperty().set(50);		
+	}
+	@FXML
+    void diceMove(KeyEvent e){
+    	switch (e.getCode()){
+			case W:
+				group.translateZProperty().set(group.getTranslateZ() + 100);
+				break;
+			case S:
+				group.translateZProperty().set(group.getTranslateZ() - 100);
+				break;
+			case Q:
+				group.rotateByX(10);
+				break;
+			case E:
+				group.rotateByX(-10);
+				break;
+			case D:
+				group.rotateByY(10);
+				break;
+			case A:
+				group.rotateByY(-10);
+				break;
+		}
+    }
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		diceMake();
+		diceRolling.setOnKeyPressed(event->{
+			switch (event.getCode()){
+			case W:
+				group.translateZProperty().set(group.getTranslateZ() + 100);
+				break;
+			case S:
+				group.translateZProperty().set(group.getTranslateZ() - 100);
+				break;
+			case Q:
+				group.rotateByX(10);
+				break;
+			case E:
+				group.rotateByX(-10);
+				break;
+			case D:
+				group.rotateByY(10);
+				break;
+			case A:
+				group.rotateByY(-10);
+				break;
+		}
+		});
 	}
 	class SmartGroup extends Group {
 
@@ -131,3 +159,6 @@ public class testController{
 			this.getTransforms().addAll(t);
 		}
 	}
+
+}
+
