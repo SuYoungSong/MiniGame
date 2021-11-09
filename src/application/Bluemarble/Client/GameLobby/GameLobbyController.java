90458d67fa7df1732fa9b7f5dfa1759e1fdca72f
SuYoungSong <prodraft@naver.com>
2021-11-09 18:42:49 +0900

    11/09
    11/09

--- src/application/Bluemarble/Client/GameLobby/GameLobbyController.java
+++ src/application/Bluemarble/Client/GameLobby/GameLobbyController.java
@@ -3,18 +3,31 @@ package application.Bluemarble.Client.GameLobby;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-public class GameLobbyController {
+public class GameLobbyController{
 
-    @FXML
-    private ScrollPane GameRoomContainer;
+    @FXML private ScrollPane GameRoomContainer;
+    @FXML private AnchorPane GameRoomCreateWindow;
+    @FXML private ScrollPane PlayerListContainer;
+    @FXML private TextField TFGameRoomCreateName;
 
-    @FXML
-    private ScrollPane PlayerListContainer;
-
+    // 게임 로비 방만들기 버튼 클릭
     @FXML
     void onClickCreateRoom(ActionEvent event) {
+    	GameRoomCreateWindow.setVisible(true);
+    }
+    
+    // 방만들기 창 방만들기 버튼 클릭
+    @FXML
+    void onClickGameRoomCreateMake(ActionEvent event) {
 
     }
-
+    // 방만들기창 취소버튼 클릭
+    @FXML
+    void onClickGameRoomCreateCancle(ActionEvent event) {
+    	GameRoomCreateWindow.setVisible(false);
+    	TFGameRoomCreateName.clear();
+    }
 }
