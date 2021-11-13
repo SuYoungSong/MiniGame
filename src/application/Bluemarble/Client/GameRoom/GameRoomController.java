4c30ca4231032bf967c66bad80d9719190f3e0a5
SuYoungSong <prodraft@naver.com>
2021-11-13 12:27:14 +0900

    11/13
    11/13

--- src/application/Bluemarble/Client/GameRoom/GameRoomController.java
+++ src/application/Bluemarble/Client/GameRoom/GameRoomController.java
@@ -1,5 +1,8 @@
 package application.Bluemarble.Client.GameRoom;
 
+import java.net.Socket;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
@@ -20,15 +23,62 @@ public class GameRoomController {
     		isDiceMake = true;
 	    	dice.diceMake(diceZone,450,400,20);
 	    	dice2.diceMake(diceZone, 350, 400, 20);
+	    	rolling(dice, dice2);
     }}
     void rolling(Dice dice, Dice dice2) {
-    	int i=0,k=0;
-		i = i + (int)(Math.random()*300);
-		k = k + (int)(Math.random()*300);
-		
-    	dice.group.rotateByX(i);
-    	dice.group.rotateByY(i);
-    	dice2.group.rotateByX(k);
-    	dice2.group.rotateByY(k);
+    	Thread thread = new Thread() {
+			public void run() {
+				Platform.runLater(()->{
+					int i=0,k=0;
+						i = i + (int)(Math.random()*10);
+						k = k + (int)(Math.random()*10);
+						dice.group.rotateByX(i);
+				    	dice.group.rotateByY	(i);
+				    	dice2.group.rotateByX(k);
+				    	dice2.group.rotateByY(k);
+				});
+			}
+		};
+    	try {
+    		thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+			thread.start();
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	
+    	
+    	
     }
 }
